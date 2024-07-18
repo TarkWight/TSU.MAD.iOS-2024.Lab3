d1368cfca0acf9a4de9d5930be4bdabcc89ccaaa
Tark Wight <arsentiy.gonda@gmail.com>
2024-07-18 14:23:06 +0500

    [Init] Initiate empty MA project

---
+++ ITindrProj/ITindrApp/ITindr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITindr
+//
+//  Created by Tark Wight on 18.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
